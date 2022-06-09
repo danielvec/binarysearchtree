002dc9029216d0ec39099c3dbc204c7f641e05fe
DanielVec <vechioladaniel@gmail.com>
2022-06-08 17:33:05 -0700

    create a node class with attributes for data, left, and right children

---
+++ main.rb
@@ -0,0 +1,10 @@
+# node class with attributes for data, left and right children
+class Node
+  attr_reader :data, :left, :right
+
+  def initialize(data = nil, left = nil, right = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
+end
