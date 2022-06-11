09712c5c826b3833d6e0d500140b4cd3ba8ef82e
DanielVec <vechioladaniel@gmail.com>
2022-06-10 21:55:53 -0700

    create a tree class that accepts an array when initalized and a method to build a balanced binary tree

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 # node class with attributes for data, left and right children
 class Node
-  attr_reader :data, :left, :right
+  attr_accessor :data, :left, :right
 
   def initialize(data = nil, left = nil, right = nil)
     @data = data
@@ -8,3 +8,24 @@ class Node
     @right = right
   end
 end
+
+
+class Tree
+  attr_accessor :root
+
+  def initialize(array)
+    build_tree(array)
+  end
+
+  def build_tree(array)
+    array = array.sort.uniq
+    return array if array.length < 2
+
+    @root = Node.new(array[array.length/2])
+    @root.left = build_tree(array[0...array.length/2])
+    @root.right = build_tree(array[array.length/2+1...array.length])
+    @root
+  end
+end
+
+t = Tree.new([1,3,7,2,8,4,5])
