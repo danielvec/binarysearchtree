07a32bf1759b307c407b7b4400aaa77b0bed0f61
DanielVec <vechioladaniel@gmail.com>
2022-06-13 20:29:59 -0700

    add method for inserting node into tree

--- main.rb
+++ main.rb
@@ -14,18 +14,35 @@ class Tree
   attr_accessor :root
 
   def initialize(array)
-    build_tree(array)
+    @root = build_tree(array)
   end
 
   def build_tree(array)
     array = array.sort.uniq
-    return array if array.length < 2
+    return if array.empty?
 
-    @root = Node.new(array[array.length/2])
-    @root.left = build_tree(array[0...array.length/2])
-    @root.right = build_tree(array[array.length/2+1...array.length])
-    @root
+    root = Node.new(array[array.length/2])
+    root.left = build_tree(array[0...array.length/2])
+    root.right = build_tree(array[array.length/2+1...array.length])
+
+    root
   end
+
+  def insert(value, root = @root)
+    return Node.new(value) if root.nil?
+
+    if root.data > value
+      root.left = insert(value, root.left)
+    elsif root.data < value
+      root.right = insert(value, root.right)
+    end
+
+    root
+  end
+
 end
 
-t = Tree.new([1,3,7,2,8,4,5])
+t = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+p t.root
+t.insert(10000)
+p t.pretty_print
